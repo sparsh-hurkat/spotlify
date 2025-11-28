0918556799ed46b9ae5542c4b4bd41a95e96f922
sparsh-hurkat <hurkatsparsh@gmail.com>
2025-11-28 01:59:23 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/spotlify/', // GitHub Pages base path
+      base: '/', // GitHub Pages base path
       server: {
         port: 3000,
         host: '0.0.0.0',
